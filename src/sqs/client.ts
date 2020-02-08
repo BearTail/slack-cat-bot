dcbaf9ddf020c4d40c630cdb8e3a93eb39df7a5b
西山希 <nishiyama@beartail.jp>
2020-02-08 14:27:47 +0900

    [Fix] sqsのクライアントを実装する

---
+++ src/sqs/client.ts
@@ -0,0 +1,33 @@
+import * as lambda from 'aws-lambda';
+import {
+  SQSClient,
+  SendMessageCommand,
+  SendMessageOutput,
+  SendMessageInput
+} from "@aws-sdk/client-sqs-node";
+
+const REGION = process.env.REGION;
+const SQS = new SQSClient({ region: REGION });
+
+export async function sendMessage(
+  body: string,
+  context: lambda.Context,
+  queueName: string,
+): Promise<SendMessageOutput | undefined> { // どうすべき？
+  const accountId = context.invokedFunctionArn.split(':')[4];
+  const queueUrl = `https://sqs.${REGION}.amazonaws.com/${accountId}/${queueName}`;
+  const params: SendMessageInput = {
+    MessageBody: body,
+    QueueUrl: queueUrl,
+  };
+  const sendMessageCommand = new SendMessageCommand(params);
+
+  try {
+    const result = await SQS.send(sendMessageCommand);
+    console.log(`Sqs result: ${result}`);
+
+    return result;
+  } catch (e) {
+    console.log(`Sqs error: ${e}`);
+  }
+}
