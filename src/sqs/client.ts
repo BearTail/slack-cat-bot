698e65de5a82337deb44ed31576e27b202481769
西山希 <nishiyama@beartail.jp>
2021-01-01 15:18:18 +0900

    [Fix] 不要なファイルの削除

--- src/sqs/client.ts
+++ src/sqs/client.ts
@@ -1,10 +1,14 @@
 import * as lambda from 'aws-lambda';
+import { randomCat } from '../actions/random-cat';
+import { randomAnimal } from '../actions/random-animal';
+import { drawAnimalOmikuji } from '../actions/animal-omikuji';
+import { gembaCat } from '../actions/gemba-cat';
 import {
   SQSClient,
   SendMessageCommand,
   SendMessageOutput,
   SendMessageInput
-} from "@aws-sdk/client-sqs-node";
+} from '@aws-sdk/client-sqs-node';
 
 const REGION = process.env.REGION;
 const SQS = new SQSClient({ region: REGION });
@@ -30,4 +34,16 @@ export async function sendMessage(
   } catch (e) {
     console.log(`Sqs error: ${e}`);
   }
+}
+
+export async function handler(event: lambda.SQSEvent): Promise<void> {
+  await Promise.all(event.Records.map(async record => {
+    const body = JSON.parse(record.body);
+    const slackText: string = body.event.text || '';
+
+    await randomCat(slackText);
+    await randomAnimal(slackText);
+    await drawAnimalOmikuji(slackText);
+    await gembaCat(slackText);
+  }));
 }
